8623c885ee043e8205eef32e5f5411f1beeb4dfe
growthhuman <growthhuman@gmail.com>
2017-08-26 20:11:57 +0900

    進むボタンでの表示まで完了

--- app/src/main/java/jp/techacademy/kenta/imabayashi/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/kenta/imabayashi/autoslideshowapp/MainActivity.java
@@ -18,7 +18,7 @@ import android.widget.ImageView;
 
 //<!--
 //        ●プロジェクトを新規作成し、 AutoSlideshowApp というプロジェクト名をつけてください
-//        ●スライドさせる画像は、Android端末に保存されているGallery画像を表示させてください（つまり、ContentProviderの利用）
+//        スライドさせる画像は、Android端末に保存されているGallery画像を表示させてください（つまり、ContentProviderの利用）
 //        ●画面には画像と3つのボタン（進む、戻る、再生/停止）を配置してください
 //        進むボタンで1つ先の画像を表示し、戻るボタンで1つ前の画像を表示します
 //        最後の画像の表示時に、進むボタンをタップすると、最初の画像が表示されるようにしてください
@@ -35,43 +35,59 @@ public class MainActivity extends AppCompatActivity {
     public static final int PERMISSIONS_REQUEST_CODE = 100;
     int fieldIndex;
 
+    ImageView imageVIew;
+    ContentResolver resolver;
+    Cursor cursor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                //ImageViewを表示して最初の画像を表示する。
+                dispImageView();
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+            }
+            // Android 5系以下の場合
+        } else {
+            //ImageViewを表示して最初の画像を表示する。
+            dispImageView();
+        }
 
-
+        //進むボタンのリスナーを作成
         Button button1 = (Button) findViewById(R.id.next_button);
-        button1.setOnClickListener(new View.OnClickListener(){
 
+        button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Log.d("DEBUG","----------------------");
-
-                // Android 6.0以降の場合
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    // パーミッションの許可状態を確認する
-                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                        // 許可されている
-                        getContentsInfo();
-                    } else {
-                        // 許可されていないので許可ダイアログを表示する
-                        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
-                    }
-
-                    // Android 5系以下の場合
-                } else {
-                    getContentsInfo();
-                }
+                getContentsInfo();
             }
         });
     }
 
     private void getContentsInfo() {
+            cursor.moveToNext();
+            //初期化は外出しして、カーソルを使いまわす。
+            fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+            Long id = cursor.getLong(fieldIndex);
+            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+            Log.d("ANDROID", "URI : " + imageUri.toString());
+
+            imageVIew = (ImageView) findViewById(R.id.imageView);
+            imageVIew.setImageURI(imageUri);
+    }
+
+    private void dispImageView(){
         // 画像の情報を取得する
-        ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
+        resolver = getContentResolver();
+        cursor = resolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                 null, // 項目(null = 全項目)
                 null, // フィルタ条件(null = フィルタなし)
@@ -79,20 +95,15 @@ public class MainActivity extends AppCompatActivity {
                 null // ソート (null ソートなし)
         );
 
-//        if (cursor.moveToFirst()) {
-        if (cursor.moveToNext()) {
-//            do {
-//            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            //初期化は外出しして、カーソルを使いまわす。
-            fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            Long id = cursor.getLong(fieldIndex);
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-            Log.d("ANDROID", "URI : " + imageUri.toString());
+        cursor.moveToFirst();
+        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+        Long id = cursor.getLong(fieldIndex);
+        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+        Log.d("ANDROID", "URI : " + imageUri.toString());
 
-            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-            imageVIew.setImageURI(imageUri);
-//            } while (cursor.moveToNext());
-        }
+        imageVIew = (ImageView) findViewById(R.id.imageView);
+        imageVIew.setImageURI(imageUri);
+
 //        cursor.close();
     }
 }
